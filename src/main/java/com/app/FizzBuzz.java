8a4153c72bde2a95ed91ee2b0ee9bf52c9429ed3
skjavab <srshryls@gamil.com>
2022-03-27 12:18:16 +0530

    One return One

---
+++ src/main/java/com/app/FizzBuzz.java
@@ -0,0 +1,11 @@
+package com.app;
+
+public class FizzBuzz {
+	
+	public static String generateResultFuzzBuzz1() {
+				
+		return "1";
+
+	}
+
+}
---
+++ src/test/java/com/app/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+	
+	@Test
+	public void oneReturnOne() {
+		Assert.assertEquals("1", FizzBuzz.generateResultFuzzBuzz1());
+	
+	}
+
+}
